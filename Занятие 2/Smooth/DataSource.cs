80706d76145d6b8fddd857f4fd8b88d3ea008e86
altuhovA <va0435224@gmail.com>
2025-04-23 21:15:21 +0300

    DataSource.cs

--- Занятие 2/Smooth/DataSource.cs
+++ Занятие 2/Smooth/DataSource.cs
@@ -1,26 +1,27 @@
-﻿using System;
+﻿﻿﻿using System;
 using System.Collections.Generic;
 
-namespace yield;
-
-public class DataSource
+namespace yield
 {
-	public static IEnumerable<DataPoint> GetData(Random random)
+	public class DataSource
 	{
-		//var dataAnalyzer = Factory.CreateAnalyzer();
-		//return dataAnalyzer.MovingMax(dataAnalyzer.MovingAverage(dataAnalyzer.SmoothExponentialy(GenerateOriginalData(random), 0.2), 20),150);
-		return GenerateOriginalData(random).SmoothExponentialy(0.2).MovingAverage(20).MovingMax(150);
-	}
-
-	public static IEnumerable<DataPoint> GenerateOriginalData(Random random)
-	{
-		var x = 0;
-		while (true)
+		public static IEnumerable<DataPoint> GetData(Random random)
 		{
-			x++;
-			var y = 10 * (1 - x / 100 % 2) + 3 * Math.Sin(x / 40.0) + 2 * random.NextDouble() - 1 + 3;
-			yield return new DataPoint(x, y);
+			//var dataAnalyzer = Factory.CreateAnalyzer();
+			//return dataAnalyzer.MovingMax(dataAnalyzer.MovingAverage(dataAnalyzer.SmoothExponentialy(GenerateOriginalData(random), 0.2), 20),150);
+			return GenerateOriginalData(random).SmoothExponentialy(0.2).MovingAverage(20).MovingMax(150);
 		}
-	}
 
+		public static IEnumerable<DataPoint> GenerateOriginalData(Random random)
+		{
+			var x = 0;
+			while (true)
+			{
+				x++;
+				var y = 10 * (1 - (x / 100) % 2) + 3 * Math.Sin(x / 40.0) + 2 * random.NextDouble() - 1 + 3;
+				yield return new DataPoint(x, y);
+			}
+		}
+
+	}
 }
